99c57b6782e0a72be34234c8a28bcec481d7b079
Aaqeb11 <aaqebahmed@gmail.com>
2024-10-20 17:47:22 +0530

    fix:image export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',  // This enables static export
+    images: { unoptimized: true },
 };
 
 export default nextConfig;
